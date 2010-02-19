a848b7836193b271879f8378218b0b218f7f6e1d
ichik <ichik@yandex.ru>
2010-02-19 12:44:39 +0300

    Adding BoP confirmation.

---
+++ tekKrush.lua
@@ -0,0 +1,9 @@
+local f = CreateFrame("Frame")
+f:RegisterEvent("CONFIRM_DISENCHANT_ROLL")
+f:RegisterEvent("LOOT_BIND_CONFIRM")
+f:SetScript("OnEvent", function(self, event, ...)
+	for i=1,STATICPOPUP_NUMDIALOGS do
+		local frame = _G["StaticPopup"..i]
+		if (frame.which == "CONFIRM_LOOT_ROLL" or frame.which == "LOOT_BIND") and frame:IsVisible() then StaticPopup_OnClick(frame, 1) end
+		end
+end)
