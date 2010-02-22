b0e7c5698765c99a42f3f8baad579099fee07eb4
ichik <ichik@yandex.ru>
2010-02-22 17:28:51 +0300

    Fixin' shit.

--- tekKrush.lua
+++ tekKrush.lua
@@ -1,9 +1,10 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("CONFIRM_DISENCHANT_ROLL")
+f:RegisterEvent("CONFIRM_LOOT_ROLL")
 f:RegisterEvent("LOOT_BIND_CONFIRM")
 f:SetScript("OnEvent", function(self, event, ...)
 	for i=1,STATICPOPUP_NUMDIALOGS do
 		local frame = _G["StaticPopup"..i]
-		if (frame.which == "CONFIRM_LOOT_ROLL" or frame.which == "LOOT_BIND") and frame:IsVisible() then StaticPopup_OnClick(frame, 1) end
-		end
+		if (frame.which == "CONFIRM_LOOT_ROLL" or frame.which == "LOOT_BIND" or frame.which == "LOOT_BIND_CONFIRM") and frame:IsVisible() then StaticPopup_OnClick(frame, 1) end
+	end
 end)
